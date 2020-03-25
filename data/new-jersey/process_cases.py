a6245d3cc01940b1bb4178e6ffecd861f06ea710
Maximilian Nickel <maxn@fb.com>
2020-03-24 21:53:01 -0700

    add NJ data

---
+++ data/new-jersey/process_cases.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from collections import defaultdict as ddict
+from datetime import datetime
+from itertools import count
+
+fout = "timeseries.h5"
+
+maxday = 20
+df = pd.read_csv("NYU-data.csv", header=2)
+df = df.sort_values(by="Start day")[:maxday]
+# df = df.dropna()
+
+counties = df.columns[2:-1]
+df["Start day"] = np.arange(len(df)) + 1
+print(df[counties])
+
+ncount = count()
+kreis_ids = ddict(ncount.__next__)
+_ns = []
+_ts = []
+_ags = []
+basedate = datetime(2020, 3, 1)
+ats = df["Start day"].to_numpy()
+print(ats)
+
+cmap = {"Morristown": "Morris"}
+
+for county in counties:
+    print(county)
+    ws = df[county].to_numpy()
+    ix = np.where(ws > 0)[0]
+    if len(ix) < 1:
+        continue
+    ts = ats[ix]
+    ws = np.diff([0] + ws[ix].tolist())
+    # print(ix, ts, ws)
+    # print(ws)
+    es = []
+    for i in range(len(ts)):
+        w = int(ws[i])
+        if w <= 0:
+            continue
+        tp = ts[i] - 1
+        # _es = np.linspace(tp, ts[i], w, endpoint=False, dtype=np.float)
+        # _es = (tp + np.abs(_es - ts[i]))[::-1] + np.random.rand(len(_es))
+        # _es = np.linspace(
+        #    max(tp, ts[i] - w * 0.01), ts[i], w, endpoint=False, dtype=np.float
+        # )
+        print(tp, ts[i], w)
+        _es = sorted(np.random.uniform(tp, ts[i], w))
+        # print(w, _es)
+        if len(es) > 0:
+            print(es[-1], _es[0], _es[-1])
+            assert es[-1] < _es[0], (_es[0], es[-1])
+        es += _es
+        # es += [ts[i]] * w
+    if len(es) > 0:
+        if county in cmap:
+            county = cmap[county]
+        kid = kreis_ids[county]
+        _ts += es
+        _ns += [kid] * len(es)
+        # _ags += [aid] * len(es)
+
+
+# _ns = [kreis_ids[k] for k in df["District"]]
+# _ts = df["Date"].values.astype(np.int64) // 10 ** 9
+# _ags = df["AGS"]
+# _ws = df["Count"]
+
+# assert len(_ts) == nevents, (len(_ts), nevents)
+knames = [None for _ in range(len(kreis_ids))]
+for kreis, i in kreis_ids.items():
+    knames[i] = kreis
+
+str_dt = h5py.special_dtype(vlen=str)
+ds_dt = h5py.special_dtype(vlen=np.dtype("int"))
+ts_dt = h5py.special_dtype(vlen=np.dtype("float32"))
+with h5py.File(fout, "w") as fout:
+    _dnames = fout.create_dataset("nodes", (len(knames),), dtype=str_dt)
+    _dnames[:] = knames
+    _cnames = fout.create_dataset("cascades", (1,), dtype=str_dt)
+    _cnames[:] = ["covid19_nj"]
+    ix = np.argsort(_ts)
+    node = fout.create_dataset("node", (1,), dtype=ds_dt)
+    node[0] = np.array(_ns, dtype=np.int)[ix]
+    time = fout.create_dataset("time", (1,), dtype=ts_dt)
+    time[0] = np.array(_ts, dtype=np.float)[ix]
+    # _agss = fout.create_dataset("ags", (len(agss),), dtype=ds_dt)
+    # _agss[:] = np.array(_ags, dtype=np.int)[ix]
+    # mark = fout.create_dataset("mark", (1,), dtype=ts_dt)
+    # mark[0] = np.array(_ws, dtype=np.float)[ix]
