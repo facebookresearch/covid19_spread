50f6c18f3c4b9f2653eb82fa5a532677d7092542
Maximilian Nickel <maxn@fb.com>
2020-04-02 22:35:58 -0700

    parallelize simulation code

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import torch.multiprocessing as mp
 import numpy as np
 import pandas as pd
+from functools import partial
 from scipy.stats import ksone
 from scipy import integrate
 from tick.hawkes import HawkesExpKern, SimuHawkesMulti
@@ -43,8 +45,27 @@ def goodness_of_fit(episode, step, mu, beta, A, nodes):
     return residuals
 
 
-def run_trial(simu, timestamps):
-    pass
+def initialize_trial(_learner, _timestamps):
+    global learner, timestamps
+    learner = _learner
+    timestamps = _timestamps
+
+
+def run_trial(t_obs, t_max, d, M, tid):
+    simu = learner._corresponding_simu()
+    simu.force_simulation = True
+    simu.verbose = False
+    simu.track_intensity(-1)
+    simu.reset()
+    simu.set_timestamps(timestamps[0], t_obs)
+    simu.end_time = t_max
+    simu.simulate()
+    simc = {i: np.zeros(M) for i in range(1, d + 1)}
+    for i in range(1, d + 1):
+        for n in range(M):
+            ix = np.where(simu.timestamps[n] < t_obs + i)[0]
+            simc[i][n] += len(ix)
+    return simc
 
 
 def simulate_mhp(t_obs, d, episode, mus, beta, A, timescale, nodes, step, trials):
@@ -60,27 +81,43 @@ def simulate_mhp(t_obs, d, episode, mus, beta, A, timescale, nodes, step, trials
     confirmed_cases = [len(t) for t in timestamps[0]]
     t_max = (t_obs + d) / timescale
     t_obs /= timescale
-    simu = learner._corresponding_simu()
-    simu.force_simulation = True
-    simu.verbose = False
-    simu.track_intensity(step)
-    simu.set_timestamps(timestamps[0], t_obs)
-    simu.end_time = t_max
+    # simu = learner._corresponding_simu()
+    # simu.force_simulation = True
+    # simu.verbose = False
+    # simu.track_intensity(-1)
+
+    with mp.Pool(
+        processes=min(trials, 40),
+        initializer=initialize_trial,
+        initargs=(learner, timestamps),
+    ) as pool:
+        ft = partial(run_trial, t_obs, t_max, d, len(nodes))
+        results = pool.map(ft, range(trials))
+    simc = {i: np.zeros(len(nodes)) for i in range(1, d + 1)}
+    for _simc in results:
+        for k, v in _simc.items():
+            simc[k] += v / trials
+
+    # simu.set_timestamps(timestamps[0], t_obs)
+    # simu.end_time = t_max
     # multi = SimuHawkesMulti(simu, n_simulations=trials, n_threads=min(trials, 40))
     # multi.simulate()
-    # print("d")
-    simc = {i: np.zeros(len(nodes)) for i in range(1, d + 1)}
-    for k in range(trials):
-        simu.reset()
-        simu.set_timestamps(timestamps[0], t_obs)
-        simu.end_time = t_max
-        simu.simulate()
-        for i in range(1, d + 1):
-            for n in range(len(nodes)):
-                ix = np.where(simu.timestamps[n] < t_obs + i)[0]
-                simc[i][n] += len(ix)
+    # print("Spectral radius =", simu.spectral_radius())
+    # print(sum(confirmed_cases))
+    # simc = {i: np.zeros(len(nodes)) for i in range(1, d + 1)}
+    # for k in range(trials):
+    #     simu.reset()
+    #     simu.set_timestamps(timestamps[0], t_obs)
+    #     # simu.max_jumps = sum(confirmed_cases) * 2
+    #     simu.end_time = t_max
+    #     simu.simulate()
+    #     for i in range(1, d + 1):
+    #         for n in range(len(nodes)):
+    #             ix = np.where(simu.timestamps[n] < t_obs + i)[0]
+    #             simc[i][n] += len(ix)
 
-    simc = {k: v / trials for k, v in simc.items()}
+    # simc = {k: v / trials for k, v in simc.items()}
+
     simc["county"] = nodes
     simc[0] = confirmed_cases
     return pd.DataFrame(simc)
