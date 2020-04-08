67be2fae50ba18c072cda4956034d635f30f82b7
Maximilian Nickel <maxn@fb.com>
2020-04-08 15:59:40 -0700

    use median to aggregate simulation trials

--- evaluation.py
+++ evaluation.py
@@ -10,6 +10,7 @@ from tick.hawkes import HawkesExpKern, SimuHawkesMulti
 from utils import to_tick_data
 from simulation_ext import HawkesExpSimulation
 
+
 def ks_critical_value(n_trials, alpha):
     """
     Critical value of Kolmogorov-Smirnov test for significance level alpha
@@ -67,23 +68,29 @@ def run_trial(t_obs, t_max, d, M, tid):
             simc[i][n] += len(ix)
     return simc
 
-def simulate_tl_mhp(t_obs, d, episode, timescale, simulator, nodes, trials, quiet=False):
+
+def simulate_tl_mhp(
+    t_obs, d, episode, timescale, simulator, nodes, trials, quiet=False
+):
     confirmed_cases = np.bincount(episode.entities, minlength=len(nodes))
     t_max = t_obs + (d / timescale)
-    counts = {i: np.zeros((len(nodes),)) for i in range(1, d+1)}
+    counts = {i: np.zeros((len(nodes),)) for i in range(1, d + 1)}
     counts[0] = confirmed_cases * trials
     for _ in range(trials):
         evts = simulator.simulate(episode.entities, episode.timestamps, t_max, quiet)
         evts = np.array(evts)
-        for i in range(1, d+1):
-            current_evts = evts[evts[:, 1] <= t_obs + (i/ timescale)]
-            print(f'len = {len(current_evts)}')
-            cur_counts = np.bincount(current_evts[:, 0].astype(int), minlength=len(nodes))
+        for i in range(1, d + 1):
+            current_evts = evts[evts[:, 1] <= t_obs + (i / timescale)]
+            print(f"len = {len(current_evts)}")
+            cur_counts = np.bincount(
+                current_evts[:, 0].astype(int), minlength=len(nodes)
+            )
             counts[i] += cur_counts
 
     counts = {k: v / trials for k, v in counts.items()}
     counts["county"] = nodes
     return pd.DataFrame(counts)
+
 
 def simulate_mhp(t_obs, d, episode, mus, beta, A, timescale, nodes, step, trials):
     """
@@ -110,10 +117,13 @@ def simulate_mhp(t_obs, d, episode, mus, beta, A, timescale, nodes, step, trials
     ) as pool:
         ft = partial(run_trial, t_obs, t_max, d, len(nodes))
         results = pool.map(ft, range(trials))
-    simc = {i: np.zeros(len(nodes)) for i in range(1, d + 1)}
+    simc = {i: [] for i in range(1, d + 1)}
+
     for _simc in results:
         for k, v in _simc.items():
-            simc[k] += v / trials
+            simc[k].append(v)
+    for k, v in simc.items():
+        simc[k] = np.median(np.stack(v, axis=0), axis=0)
 
     # simu.set_timestamps(timestamps[0], t_obs)
     # simu.end_time = t_max
