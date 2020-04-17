e1a4e4b0eb050cbf4d2e6b14d0fd728541b991cb
lematt1991 <lematt1991@gmail.com>
2020-04-17 10:44:58 -0700

    Add function to drop days from HDF5 dataset

--- common.py
+++ common.py
@@ -3,6 +3,56 @@ import h5py
 import numpy as np
 import torch as th
 from numpy.linalg import norm
+import itertools
+from collections import defaultdict
+import datetime
+
+
+def drop_k_days(dset, outfile, days):
+    # We'll need to rename everything in case the number of events for a given dimension
+    # drops to 0 after filtering.  
+    counter = itertools.count()
+    namer = defaultdict(counter.__next__)
+    ds_dt = h5py.special_dtype(vlen=np.dtype("int"))
+    ts_dt = h5py.special_dtype(vlen=np.dtype("float32"))
+
+    with h5py.File(dset, 'r') as hin, h5py.File(outfile, 'w') as hout:
+        times = hin['time'][:]
+        nodes = hin['node'][:]
+        new_nodes = []
+        new_times = []
+        # Compute the new maxtime
+        max_time = max(*[ts[-1].item() for ts in times]) - days
+
+        processed = np.array([])
+        all_times = []
+        all_nodes = []
+
+        for idx, ts in enumerate(times):
+            mask = ts < max_time
+            new_nodes.append(np.array([namer[x] for x in nodes[idx][mask]]))
+            new_times.append(ts[mask])
+
+            mask = ~np.in1d(new_nodes[-1], processed)
+            all_nodes.append(new_nodes[-1][mask])
+            all_times.append(new_times[-1][mask])
+            processed = np.concatenate([processed, np.unique(all_nodes[-1])])
+
+        old_idxs, new_idxs = map(np.array, zip(*namer.items()))
+        hout['nodes'] = hin['nodes'][:][old_idxs[sorted(new_idxs)]]
+        hout.create_dataset('time', data=new_times, dtype=ts_dt)
+        hout.create_dataset('node', data=new_nodes, dtype=ds_dt)
+        hout['cascades'] = hin['cascades'][:]
+        if 'basedate' in hin.attrs:
+            date = datetime.datetime.strptime(hin.attrs['basedate'], '%Y-%m-%d')
+            hout.attrs['basedate'] = str((date - datetime.timedelta(days=days)).date())
+        
+        all_times = np.concatenate(all_times)
+        idx = all_times.argsort()
+        all_times = all_times[idx]
+        all_nodes = np.concatenate(all_nodes)[idx]
+        hout['all_nodes'] = all_nodes
+        hout['all_times'] = all_times
 
 
 def load_model(model_path, M):
