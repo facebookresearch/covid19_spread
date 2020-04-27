9af698b9dae15a8ac313e5fbd7bf033a4db380ac
lematt1991 <lematt1991@gmail.com>
2020-04-27 12:13:27 -0700

    Add remote and sweep support

--- cv.py
+++ cv.py
@@ -8,35 +8,23 @@ import torch as th
 import yaml
 from argparse import Namespace
 from datetime import datetime
-
+from typing import Dict, Any
 import common
 import metrics
+import itertools
+import copy
+import random
+from functools import partial
+import submitit
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config", help="config file (yaml)")
-    parser.add_argument("module", help="config file (yaml)")
-    parser.add_argument("-basedate", help="Base date for forecasting")
-    parser.add_argument(
-        "-remote", action="store_true", help="Run jobs remotely on SLURM"
-    )
-    parser.add_argument("-timeout-min", type=int, default=12 * 60)
-    parser.add_argument("-array-parallelism", type=int, default=50)
-    parser.add_argument("-mem-gb", type=int, default=20)
-    parser.add_argument("-ncpus", type=int, default=20)
-    opt = parser.parse_args()
+def cv(basedir: str, cfg: Dict[str, Any]):
+    try:
+        basedir = basedir.replace('%j', submitit.JobEnvironment().job_id)
+    except Exception:
+        pass  # running locally, basedir is fine...
 
-    now = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    user = os.environ["USER"]
-
-    cfg = yaml.load(open(opt.config), Loader=yaml.FullLoader)
-
-    if opt.remote:
-        basedir = f"/checkpoint/{user}/covid19/forecasts/{region}/{now}"
-    else:
-        basedir = "/tmp"
-
+    os.makedirs(basedir, exist_ok=True)
     def _path(path):
         return os.path.join(basedir, path)
 
@@ -63,10 +51,73 @@ if __name__ == "__main__":
     df_val.to_csv(_path(cfg["metrics"]["output"]))
     print(df_val)
 
-    # -- prediction interval --
+        # -- prediction interval --
     with th.no_grad():
         df_mean, df_std = mod.run_prediction_interval(
             train_params, cfg["prediction_interval"]["nsamples"], model
         )
     df_mean.to_csv(_path(cfg["prediction_interval"]["output_mean"]))
     df_std.to_csv(_path(cfg["prediction_interval"]["output_std"]))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="config file (yaml)")
+    parser.add_argument("module", help="config file (yaml)")
+    parser.add_argument("-basedate", help="Base date for forecasting")
+    parser.add_argument(
+        "-remote", action="store_true", help="Run jobs remotely on SLURM"
+    )
+    parser.add_argument("-timeout-min", type=int, default=12 * 60)
+    parser.add_argument("-array-parallelism", type=int, default=50)
+    parser.add_argument("-mem-gb", type=int, default=20)
+    parser.add_argument("-ncpus", type=int, default=20)
+    parser.add_argument("-max-jobs", type=int, default=200)
+    opt = parser.parse_args()
+
+    now = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    user = os.environ["USER"]
+
+    cfg = yaml.load(open(opt.config), Loader=yaml.FullLoader)
+    region = cfg["region"]
+    
+    if opt.remote:
+        basedir = f"/checkpoint/{user}/covid19/forecasts/{region}/{now}"
+    else:
+        basedir = "/tmp"
+
+
+    cfgs = []
+    sweep_params = [k for k, v in cfg[opt.module]['train'].items() if isinstance(v, list)]
+    if len(sweep_params) == 0:
+        cfgs.append(cfg)
+    else:
+        random.seed(0)
+        for vals in itertools.product(*[cfg[opt.module]['train'][k] for k in sweep_params]):
+            clone = copy.deepcopy(cfg)
+            clone[opt.module]['train'].update({k: v for k, v in zip(sweep_params, vals)})
+            cfgs.append(clone)
+        random.shuffle(cfgs)
+        cfgs = cfgs[:opt.max_jobs]
+
+    if opt.remote:
+        ngpus = cfg[opt.module].get('resources', {}).get('gpus', 0)
+        ncpus = cfg[opt.module].get('resources', {}).get('cpus', 3)
+        memgb = cfg[opt.module].get('resources', {}).get('memgb', 20)
+        executor = submitit.AutoExecutor(folder=basedir + '/%j')
+        executor.update_parameters(
+            name=f'cv_{region}',
+            gpus_per_node=ngpus,
+            cpus_per_task=ncpus,
+            mem_gb=memgb,
+            slurm_array_parallelism=opt.array_parallelism,
+            timeout_min=12 * 60,
+        )
+        launcher = executor.map_array
+        basedirs = [f'{basedir}/%j' for _ in cfgs]
+    else:
+        basedirs = [os.path.join(basedir, f'job_{i}') for i in range(len(cfgs))]
+        launcher = map
+
+    list(launcher(cv, basedirs, cfgs))
+    print(basedir)
