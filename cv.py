b6f77f87c668155876a1f946551457b413aad93e
Maximilian Nickel <maxn@fb.com>
2020-05-07 14:13:35 -0700

    fix submitt params

--- cv.py
+++ cv.py
@@ -25,29 +25,30 @@ import tempfile
 import shutil
 
 
-BestRun = namedtuple('BestRun', ('pth', 'name'))
+BestRun = namedtuple("BestRun", ("pth", "name"))
+
 
 class CV:
     def run_simulate(self, dset, train_params, model, sim_params):
         ...
-    
+
     def run_prediction_interval(self, args, nsamples, model=None):
         ...
 
     def run_train(self, dset, model_params, model_out):
         ...
-    
+
     def model_selection(self, basedir: str) -> List[BestRun]:
-        best_run, best_MAE = None, float('inf')
-        for metrics_pth in glob(os.path.join(basedir, '*/metrics.csv')):
-            metrics = pd.read_csv(metrics_pth, index_col='Measure')
-            if metrics.loc['MAE'].values[-1] < best_MAE:
-                best_MAE = metrics.loc['MAE'].values[-1]
+        best_run, best_MAE = None, float("inf")
+        for metrics_pth in glob(os.path.join(basedir, "*/metrics.csv")):
+            metrics = pd.read_csv(metrics_pth, index_col="Measure")
+            if metrics.loc["MAE"].values[-1] < best_MAE:
+                best_MAE = metrics.loc["MAE"].values[-1]
                 best_run = os.path.dirname(metrics_pth)
-        return [BestRun(best_run, 'best_mae')]
+        return [BestRun(best_run, "best_mae")]
 
 
-def run_cv(module: str, basedir: str, cfg: Dict[str, Any], prefix=''):
+def run_cv(module: str, basedir: str, cfg: Dict[str, Any], prefix=""):
     try:
         basedir = basedir.replace("%j", submitit.JobEnvironment().job_id)
     except Exception:
@@ -77,8 +78,10 @@ def run_cv(module: str, basedir: str, cfg: Dict[str, Any], prefix=''):
 
     # -- simulate --
     with th.no_grad():
-        sim_params = cfg[module].get('simulate', {})
-        df_forecast = mod.run_simulate(val_in, train_params, model, sim_params=sim_params)
+        sim_params = cfg[module].get("simulate", {})
+        df_forecast = mod.run_simulate(
+            val_in, train_params, model, sim_params=sim_params
+        )
     print(f"Storing validation in {val_out}")
     df_forecast.to_csv(val_out)
 
@@ -115,7 +118,11 @@ def mk_absolute_paths(cfg):
     elif isinstance(cfg, list):
         return list(map(mk_absolute_paths, cfg))
     else:
-        return os.path.realpath(cfg) if isinstance(cfg, str) and os.path.exists(cfg) else cfg
+        return (
+            os.path.realpath(cfg)
+            if isinstance(cfg, str) and os.path.exists(cfg)
+            else cfg
+        )
 
 
 def load_config(cfg_pth: str) -> Dict[str, Any]:
@@ -133,7 +140,7 @@ def run_best(basedir, cfg, module):
     best_runs = mod.model_selection(basedir)
 
     cfg = copy.deepcopy(cfg)
-    cfg['validation']['days'] = 0
+    cfg["validation"]["days"] = 0
 
     ngpus = cfg[module].get("resources", {}).get("gpus", 0)
     ncpus = cfg[module].get("resources", {}).get("cpus", 3)
@@ -141,9 +148,9 @@ def run_best(basedir, cfg, module):
     timeout = cfg[module].get("resources", {}).get("timeout", 12 * 60)
 
     for run in best_runs:
-        job_config = load_config(os.path.join(run.pth, module + '.yml'))
+        job_config = load_config(os.path.join(run.pth, module + ".yml"))
         cfg[module] = job_config
-        cfg["validation"]["output"] = run.name + '_forecast.csv'
+        cfg["validation"]["output"] = run.name + "_forecast.csv"
         launcher = cv
         if opt.remote:
             executor = submitit.AutoExecutor(folder=run.pth)
@@ -155,13 +162,13 @@ def run_best(basedir, cfg, module):
                 timeout_min=timeout,
             )
             launcher = partial(executor.submit, cv)
-        launcher(module, run.pth, cfg, prefix='final_model_')
+        launcher(module, run.pth, cfg, prefix="final_model_")
 
 
-
-@click.group(cls=DefaultGroup, default_command='cv')
+@click.group(cls=DefaultGroup, default_command="cv")
 def cli():
     pass
+
 
 @cli.command()
 @click.argument("config")
@@ -172,9 +179,9 @@ def cli():
 @click.option("-max-jobs", type=click.INT, default=200)
 @click.option("-basedir", default=None, help="Path to sweep base directory")
 def cv(config, module, validate_only, remote, array_parallelism, max_jobs, basedir):
-    '''
+    """
     Run cross validation pipeline for a given module.
-    '''
+    """
     now = datetime.now().strftime("%Y_%m_%d_%H_%M")
     user = os.environ["USER"]
 
@@ -189,32 +196,25 @@ def cv(config, module, validate_only, remote, array_parallelism, max_jobs, based
 
     os.makedirs(basedir, exist_ok=True)
 
-
     # Copy the dataset into the basedir
-    shutil.copy(cfg[module]['data'], basedir)
-    cfg[module]['data'] = os.path.join(basedir, os.path.basename(cfg[module]['data']))
+    shutil.copy(cfg[module]["data"], basedir)
+    cfg[module]["data"] = os.path.join(basedir, os.path.basename(cfg[module]["data"]))
 
-    with open(os.path.join(basedir, 'cfg.yml'), 'w') as fout:
+    with open(os.path.join(basedir, "cfg.yml"), "w") as fout:
         yaml.dump(cfg, fout)
 
     cfgs = []
-    sweep_params = [
-        k for k, v in cfg[module]["train"].items() if isinstance(v, list)
-    ]
+    sweep_params = [k for k, v in cfg[module]["train"].items() if isinstance(v, list)]
     if len(sweep_params) == 0:
         cfgs.append(cfg)
     else:
         random.seed(0)
-        for vals in itertools.product(
-            *[cfg[module]["train"][k] for k in sweep_params]
-        ):
+        for vals in itertools.product(*[cfg[module]["train"][k] for k in sweep_params]):
             clone = copy.deepcopy(cfg)
-            clone[module]["train"].update(
-                {k: v for k, v in zip(sweep_params, vals)}
-            )
+            clone[module]["train"].update({k: v for k, v in zip(sweep_params, vals)})
             cfgs.append(clone)
         random.shuffle(cfgs)
-        cfgs = cfgs[: max_jobs]
+        cfgs = cfgs[:max_jobs]
 
     if remote:
         ngpus = cfg[module].get("resources", {}).get("gpus", 0)
@@ -227,7 +227,7 @@ def cv(config, module, validate_only, remote, array_parallelism, max_jobs, based
             gpus_per_node=ngpus,
             cpus_per_task=ncpus,
             mem_gb=memgb,
-            slurm_array_parallelism=array_parallelism,
+            array_parallelism=array_parallelism,
             timeout_min=timeout,
         )
         launcher = executor.map_array
@@ -236,29 +236,39 @@ def cv(config, module, validate_only, remote, array_parallelism, max_jobs, based
         basedirs = [os.path.join(basedir, f"job_{i}") for i in range(len(cfgs))]
         launcher = map
 
-    with snapshot.SnapshotManager(snapshot_dir=basedir + "/snapshot", with_submodules=True):
+    with snapshot.SnapshotManager(
+        snapshot_dir=basedir + "/snapshot", with_submodules=True
+    ):
         jobs = list(launcher(partial(run_cv, module), basedirs, cfgs))
 
         # Find the best model and retrain on the full dataset
         launcher = run_best
         if remote:
             executor = submitit.AutoExecutor(folder=basedir)
-            executor.update_parameters(name="model_selection", cpus_per_task=1, mem_gb=2, timeout_min=20)
+            executor.update_parameters(
+                name="model_selection", cpus_per_task=1, mem_gb=2, timeout_min=20
+            )
             # Launch the model selection job *after* the sweep finishs
-            sweep_job = jobs[0].job_id.split('_')[0]
-            executor.update_parameters(slurm_additional_parameters={'dependency': f'afterany:{sweep_job}'})
+            sweep_job = jobs[0].job_id.split("_")[0]
+            executor.update_parameters(
+                additional_parameters={"dependency": f"afterany:{sweep_job}"}
+            )
             launcher = partial(executor.submit, run_best) if remote else run_best
         if not validate_only:
             launcher(basedir, cfg, module)
 
     print(basedir)
-    
+
 
 @cli.command()
-@click.argument('config_pth')
-@click.argument('module')
-@click.option('-period', type=int, help='Number of days for sliding window', required=True)
-@click.option('-start-date', type=click.DateTime(), default='2020-04-01', help='Start date')
+@click.argument("config_pth")
+@click.argument("module")
+@click.option(
+    "-period", type=int, help="Number of days for sliding window", required=True
+)
+@click.option(
+    "-start-date", type=click.DateTime(), default="2020-04-01", help="Start date"
+)
 @click.option("-validate-only", type=click.BOOL, default=False)
 @click.option("-remote", type=click.BOOL, default=False)
 @click.option("-array-parallelism", type=click.INT, default=50)
@@ -274,39 +284,46 @@ def backfill(
     validate_only: bool = False,
     remote: bool = False,
     array_parallelism: int = 50,
-    max_jobs: int = 200
+    max_jobs: int = 200,
 ):
-    '''
+    """
     Run the cross validation pipeline over multiple time points.
-    '''
+    """
     config = load_config(config_pth)
-    assert dates is not None or period is not None, "Must specify either dates or period"
-    gt = metrics.load_ground_truth(config[module]['data'])
+    assert (
+        dates is not None or period is not None
+    ), "Must specify either dates or period"
+    gt = metrics.load_ground_truth(config[module]["data"])
     if dates is None:
-        dates = pd.date_range(start=start_date, end=gt.index.max(), freq=f'{period}D', closed='left')
-
+        dates = pd.date_range(
+            start=start_date, end=gt.index.max(), freq=f"{period}D", closed="left"
+        )
 
     now = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    basedir = f'/checkpoint/{os.environ["USER"]}/covid19/forecasts/{config["region"]}/{now}'
-    print(f'Backfilling in {basedir}')
-    ext = os.path.splitext(config[module]['data'])[-1]
+    basedir = (
+        f'/checkpoint/{os.environ["USER"]}/covid19/forecasts/{config["region"]}/{now}'
+    )
+    print(f"Backfilling in {basedir}")
+    ext = os.path.splitext(config[module]["data"])[-1]
     for date in dates:
-        print(f'Running CV for {date.date()}')
+        print(f"Running CV for {date.date()}")
         with tempfile.TemporaryDirectory() as tdir:
-            tfile = os.path.join(tdir, os.path.basename(config[module]['data']))
-            tconfig = os.path.join(tdir, 'config.yml')
+            tfile = os.path.join(tdir, os.path.basename(config[module]["data"]))
+            tconfig = os.path.join(tdir, "config.yml")
             days = (gt.index.max() - date).days
-            filter_validation_days(config[module]['data'], tfile, days)
+            filter_validation_days(config[module]["data"], tfile, days)
             current_config = copy.deepcopy(config)
-            current_config[module]['data'] = tfile
-            with open(tconfig, 'w') as fout:
+            current_config[module]["data"] = tfile
+            with open(tconfig, "w") as fout:
                 yaml.dump(current_config, fout)
-            cv_params = {k: v for k, v in ctx.params.items() if k in {p.name for p in cv.params}}
+            cv_params = {
+                k: v for k, v in ctx.params.items() if k in {p.name for p in cv.params}
+            }
             ctx.invoke(
                 cv,
                 config=tconfig,
-                basedir=os.path.join(basedir, f'sweep_{date.date()}'),
-                **cv_params
+                basedir=os.path.join(basedir, f"sweep_{date.date()}"),
+                **cv_params,
             )
 
 
