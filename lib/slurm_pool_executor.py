cf246a35b17057d97413912573550efccef21c56
lematt1991 <lematt1991@gmail.com>
2020-07-27 07:51:46 -0700

    Updating slurm pool executor

--- lib/slurm_pool_executor.py
+++ lib/slurm_pool_executor.py
@@ -12,6 +12,7 @@ import os
 import sqlite3
 from functools import partial
 import enum
+import random
 import re
 from contextlib import (
     contextmanager,
@@ -20,6 +21,7 @@ from contextlib import (
     AbstractContextManager,
 )
 import traceback
+import hashlib
 
 
 @contextmanager
@@ -35,14 +37,15 @@ def env_var(key_vals: tp.Dict[str, tp.Union[str, None]]):
     old_dict = {k: os.environ.get(k, None) for k in key_vals.keys()}
     for k, v in key_vals.items():
         if v is None:
-            del os.environ[k]
+            if k in os.environ:
+                del os.environ[k]
         else:
             os.environ[k] = v
     yield
     for k, v in old_dict.items():
         if v:
             os.environ[k] = v
-        else:
+        elif k in os.environ:
             del os.environ[k]
 
 
@@ -60,39 +63,54 @@ class TransactionManager(AbstractContextManager):
         self.conn = None
         self.nesting = 0
 
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["conn"]
+        print(f"get State! nesting={self.nesting}")
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        print(f"set State! nesting={self.nesting}")
+        self.conn = None
+
     def __enter__(self):
         if self.conn is None:
-            self.conn = sqlite3.connect(self.db_url, timeout=30)
-            self.conn.isolation_level = "EXCLUSIVE"
-            self.conn.execute("BEGIN EXCLUSIVE")
+            exn = None
+            # If there's a lot of contention, we may need to handle a timeout exception
+            for _ in range(25):
+                try:
+                    print(f"Beginning transaction, nesting={self.nesting}")
+                    self.conn = sqlite3.connect(
+                        self.db_url, timeout=random.randint(20, 30)
+                    )
+                    self.conn.isolation_level = "EXCLUSIVE"
+                    self.conn.execute("BEGIN EXCLUSIVE")
+                    exn = None
+                    break
+                except sqlite3.OperationalError as e:
+                    print(f"Failed to execute transaction!")
+                    traceback.print_exc(file=sys.stderr)
+                    self.conn.rollback()
+                    self.conn.close()
+                    self.conn = None
+                    time.sleep(1)
+                    exn = e
+            if exn is not None:
+                print("Failed too many times!")
+                raise exn
         self.nesting += 1
-        return self
+        return self.conn
 
     def __exit__(self, *args, **kwargs):
         self.nesting -= 1
         if self.nesting == 0:
+            print("Committing transaction")
             self.conn.commit()
             self.conn.close()
             self.conn = None
-        if self.exn is not None:
-            raise self.exn
-
-    def execute(self, query, *args):
-        res = None
-        assert self.conn is not None, "Executing outside of transaction!!!"
-        for _ in range(self.retries):
-            try:
-                res = self.conn.execute(query, *args)
-                self.exn = None
-                break
-            except sqlite3.OperationalError as e:
-                print(f"Failed to execute transaction!", file=sys.stderr)
-                print(e, file=sys.stderr)
-                time.sleep(1)
-                self.exn = e
-        if self.exn is not None:
-            raise self.exn
-        return res
+        else:
+            print(f"Decreasing nesting to {self.nesting}")
 
 
 class JobStatus(enum.IntEnum):
@@ -106,97 +124,143 @@ class JobStatus(enum.IntEnum):
             return self.value
 
 
-def worker(db_pth, worker_id):
-    """
-    Worker function.  This function actually gets submitted to the cluster.  
-    It repeatedly looks for pending jobs without any unfinished dependencies and 
-    runs them.  
-    """
-    worker_env = job_environment.JobEnvironment()
-    running_status = len(JobStatus) + worker_id + 1  # mark in progress with this code
-    transaction_manager = TransactionManager(db_pth)
-    while True:
-        with transaction_manager as conn:
-            # Select a pending job that doesn't have any unfinished dependencies
-            res = conn.execute(
-                f"""
-            SELECT jobs.pickle, jobs.job_id
-            FROM jobs 
-            LEFT JOIN dependencies USING(pickle)
-            LEFT JOIN jobs j2 ON dependencies.depends_on=j2.pickle
-            WHERE (
-                COALESCE(j2.status, {JobStatus.success.value})={JobStatus.success.value}
-                OR COALESCE(j2.status, {JobStatus.failure.value})={JobStatus.failure.value}
-            ) AND jobs.status={JobStatus.pending.value}
-            LIMIT 1
-            """
-            ).fetchall()
-            if len(res) == 0:  # no jobs left
-                status = (JobStatus.success, JobStatus.failure, JobStatus.final)
-                unfinished = conn.execute(
-                    f"SELECT COUNT(1) FROM jobs WHERE status NOT IN (?,?,?)", status
-                )
-                if unfinished.fetchone()[0] > 0:
-                    # Other jobs are still running, just exit...
-                    print(f"Worker {worker_id} is finished!")
-                    return
-                # All other workers have exited.  Check if there are any final jobs to execute...
-                res = conn.execute(
-                    f"SELECT pickle, job_id FROM jobs WHERE status={JobStatus.final} LIMIT 1"
-                ).fetchall()
-                if len(res) == 0:
-                    # No final jobs to execute, exit...
-                    print(f"Worker {worker_id} is finished!")
-                    return
-                print(f"Worker {worker_id} is executing final_job: {res[0][0]}")
+class Worker:
+    def __init__(self, db_pth: str, worker_id: int):
+        self.db_pth = db_pth
+        self.worker_id = worker_id
+        self.sleep = 0
 
-            [(pickle, job_id)] = res
-            # Mark that we're working on this job.
-            res = conn.execute(
-                "UPDATE jobs SET status=?, worker_id=? WHERE pickle=? AND status=?",
-                (running_status, worker_env.job_id, pickle, JobStatus.pending),
-            )
+    def fetch_ready_job(self, conn):
+        # Select a pending job that doesn't have any unfinished dependencies
+        return conn.execute(
+            f"""
+        SELECT 
+            jobs.pickle, 
+            jobs.job_id, 
+            MIN(COALESCE(j2.status, {JobStatus.success})) as min_status, 
+            MAX(COALESCE(j2.status, {JobStatus.failure})) AS max_status
+        FROM jobs 
+        LEFT JOIN dependencies USING(pickle)
+        LEFT JOIN jobs j2 ON dependencies.depends_on=j2.pickle
+        WHERE jobs.status={JobStatus.pending}
+        GROUP BY jobs.pickle, jobs.job_id 
+            HAVING min_status >= {JobStatus.success} AND max_status <= {JobStatus.failure}
+        LIMIT 1
+        """
+        ).fetchall()
 
-        # Run the job
-        # Some environment variable trickery to get submitit to find the correct pickle file
-        env_vars = {
-            "SLURM_JOB_ID": job_id,
-            "SLURM_ARRAY_JOB_ID": None,
-            "SLURM_ARRAY_TASK_ID": None,
-        }
-        if re.match(r"job_(\d+)", job_id):
-            env_vars["SLURM_ARRAY_JOB_ID"] = "job"
-            env_vars["SLURM_ARRAY_TASK_ID"] = re.search(r"job_(\d+)", job_id).group(1)
-        with env_var(
-            env_vars
-        ):  # will reset os.environ when leaving the context manager
-            job_dir = os.path.dirname(pickle)
-            env = job_environment.JobEnvironment()
-            paths = utils.JobPaths(job_dir, job_id=env.job_id, task_id=env.global_rank)
-            with paths.stderr.open("w", buffering=1) as stderr, paths.stdout.open(
-                "w", buffering=1
-            ) as stdout:
-                with redirect_stderr(stderr), redirect_stdout(stdout):
-                    try:
-                        process_job(job_dir)
-                        status = JobStatus.success
-                    except Exception:
-                        status = JobStatus.failure
-                        traceback.print_exc(file=sys.stderr)
+    def finished(self, conn):
+        status = (JobStatus.success, JobStatus.failure)
+        return (
+            conn.execute(
+                "SELECT COUNT(1) FROM jobs WHERE status NOT IN (?,?)", status
+            ).fetchone()[0]
+            == 0
+        )
 
-            print(f"Worker {worker_id} finished job with status {status}")
+    def count_running(self, conn):
+        return conn.execute(
+            f"SELECT COUNT(1) FROM jobs WHERE status > {len(JobStatus)}"
+        ).fetchone()[0]
+
+    def get_final_jobs(self, conn):
+        return conn.execute(
+            "SELECT pickle, job_id FROM jobs WHERE status=? LIMIT 1", (JobStatus.final,)
+        ).fetchall()
+
+    def run(self):
+        worker_job_id = f"worker_{self.worker_id}"
+        running_status = (
+            len(JobStatus) + self.worker_id + 1
+        )  # mark in progress with this code
+        transaction_manager = TransactionManager(self.db_pth)
+        while True:
+            if self.sleep > 0:
+                print(f"Sleeping...")
+                time.sleep(self.sleep)
             with transaction_manager as conn:
-                conn.execute(
-                    f"UPDATE jobs SET status={status.value} WHERE pickle=?", (pickle,)
+                ready = self.fetch_ready_job(conn)
+                if len(ready) == 0:  # no jobs ready
+                    if self.finished(conn):
+                        return  # all jobs are finished, exiting...
+
+                    if self.count_running(conn) > 0:
+                        self.sleep = min(max(self.sleep * 2, 1), 30)
+                        continue
+
+                    ready = self.get_final_jobs(conn)
+                    if len(ready) == 0:
+                        self.sleep = min(max(self.sleep * 2, 1), 30)
+                        continue
+                    print(
+                        f"Worker {self.worker_id} is executing final_job: {ready[0][0]}"
+                    )
+
+                pickle, job_id = ready[0][0], ready[0][1]
+                # Mark that we're working on this job.
+                res = conn.execute(
+                    "UPDATE jobs SET status=?, worker_id=? WHERE pickle=? AND status=?",
+                    (running_status, worker_job_id, pickle, JobStatus.pending),
                 )
+
+            # Run the job
+            # Some environment variable trickery to get submitit to find the correct pickle file
+            env_vars = {
+                "SLURM_JOB_ID": job_id,
+                "SLURM_ARRAY_JOB_ID": None,
+                "SLURM_ARRAY_TASK_ID": None,
+            }
+            if re.match(r"job_(\d+)", job_id):
+                env_vars["SLURM_ARRAY_JOB_ID"] = "job"
+                env_vars["SLURM_ARRAY_TASK_ID"] = re.search(r"job_(\d+)", job_id).group(
+                    1
+                )
+            with env_var(
+                env_vars
+            ):  # will reset os.environ when leaving the context manager
+                job_dir = os.path.dirname(pickle)
+                env = job_environment.JobEnvironment()
+                paths = utils.JobPaths(
+                    job_dir, job_id=env.job_id, task_id=env.global_rank
+                )
+                with paths.stderr.open("w", buffering=1) as stderr, paths.stdout.open(
+                    "w", buffering=1
+                ) as stdout:
+                    with redirect_stderr(stderr), redirect_stdout(stdout):
+                        try:
+                            process_job(job_dir)
+                            status = JobStatus.success
+                        except Exception:
+                            status = JobStatus.failure
+                            traceback.print_exc(file=sys.stderr)
+
+                print(f"Worker {self.worker_id} finished job with status {status}")
+                with transaction_manager as conn:
+                    conn.execute(
+                        f"UPDATE jobs SET status={status.value} WHERE pickle=?",
+                        (pickle,),
+                    )
 
 
 class SlurmPoolExecutor(SlurmExecutor):
     def __init__(self, *args, **kwargs):
+        db_pth = kwargs.pop("db_pth", None)
         super().__init__(*args, **kwargs)
         self.nested = False
         os.makedirs(self.folder, exist_ok=True)
-        self.db_pth = os.path.join(str(self.folder), ".job.db")
+
+        if db_pth is None:
+            # Place the actual database in ~/.slurm_pool/<unique_id>.db
+            m = hashlib.md5()
+            m.update(str(self.folder).encode("utf-8"))
+            unique_filename = m.hexdigest()
+            self.db_pth = os.path.expanduser(f"~/.slurm_pool/{unique_filename}.db")
+            os.makedirs(os.path.dirname(self.db_pth), exist_ok=True)
+            if not os.path.exists(os.path.join(str(self.folder), ".job.db")):
+                os.symlink(self.db_pth, os.path.join(str(self.folder), ".job.db"))
+        else:
+            self.db_pth = db_pth
+
         self.transaction_manager = TransactionManager(self.db_pth)
 
         with self.transaction_manager as conn:
@@ -206,15 +270,6 @@ class SlurmPoolExecutor(SlurmExecutor):
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS dependencies(pickle text, depends_on text)"
             )
-
-    def __getstate__(self):
-        state = self.__dict__.copy()
-        del state["transaction_manager"]
-        return state
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-        self.transaction_manager = TransactionManager(self.db_pth)
 
     def _submit_command(self, command):
         tmp_uuid = uuid.uuid4().hex
@@ -298,7 +353,10 @@ class SlurmPoolExecutor(SlurmExecutor):
             workers = min(workers, njobs)
             ex = SlurmExecutor(folder or self.folder)
             ex.update_parameters(**self.parameters)
-            ex.map_array(partial(worker, self.db_pth), list(range(workers)))
+            ex.map_array(
+                lambda x: x.run(), [Worker(self.db_pth, i) for i in range(workers)]
+            )
+            # ex.map_array(partial(worker, self.db_pth), list(range(workers)))
 
     @contextmanager
     def nest(self):
