de84391619fffafb666ac39cab6633c98e811490
Maximilian Nickel <maxn@fb.com>
2020-08-09 08:48:15 -0700

    normalize weather data to 0-1

--- data/usa/google/process_open_data.py
+++ data/usa/google/process_open_data.py
@@ -1,6 +1,5 @@
 import pandas
 
-
 index = pandas.read_csv("https://storage.googleapis.com/covid19-open-data/v2/index.csv")
 index = index[index["country_code"] == "US"]
 
@@ -24,12 +23,22 @@ cols = ["average_temperature", "minimum_temperature", "maximum_temperature", "ra
 
 weather_piv = weather.pivot(index="date", values=cols, columns="loc")
 
+# normalize to 0-1
+for col in cols[:-1]:
+    min_temp = weather_piv[col].min()  # .values.min()
+    weather_piv[col] = weather_piv[col].values - (min_temp.values.min() - 1)
+    max_temp = weather_piv[col].max()  # .values.max()
+    weather_piv[col] = weather_piv[col].values / max_temp.values.max()
+    print(weather_piv[col])
+weather_piv["rainfall"] /= weather_piv["rainfall"].max().values.max()
+print(weather_piv["rainfall"])
+
 # Transform into z-scores
-weather_piv = (weather_piv - weather_piv.mean()) / weather_piv.std(skipna=True)
-weather_piv.iloc[0] = weather_piv.iloc[0].fillna(0)
+# weather_piv = (weather_piv - weather_piv.mean()) / weather_piv.std(skipna=True)
+# weather_piv.iloc[0] = weather_piv.iloc[0].fillna(0)
 weather_piv = weather_piv.fillna(method="ffill")
 
 weather_piv = weather_piv.transpose().unstack(0).transpose()
 weather_piv = weather_piv.stack().unstack(0).reset_index(0)
 weather_piv = weather_piv.rename(columns={"level_0": "type"})
-weather_piv.to_csv("weather_features.csv", index_label="region")
+weather_piv.round(3).to_csv("weather_features.csv", index_label="region")
