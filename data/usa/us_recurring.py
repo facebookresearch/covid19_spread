dafe7e7b047a623b3bc328f6ae7a3622fa66a627
lematt1991 <lematt1991@gmail.com>
2020-10-28 06:35:29 -0700

    Adding check that data becomes available each day before 8PM EST to US recurring

--- data/usa/us_recurring.py
+++ data/usa/us_recurring.py
@@ -14,6 +14,7 @@ import argparse
 from subprocess import check_call, check_output
 import pandas
 from lib.slack import get_client as get_slack_client
+from datetime import date, datetime, timedelta
 
 
 MAIL_TO = ["mattle@fb.com"]
@@ -43,6 +44,12 @@ class USARRecurring(recurring.Recurring):
 
     def latest_date(self):
         df = pandas.read_csv("data_cases.csv", index_col="region")
+        max_date = pandas.to_datetime(df.columns).max().date()
+        if max_date < (date.today() - timedelta(days=1)) and datetime.now().hour > 17:
+            expected_date = date.today() - timedelta(days=1)
+            client = get_slack_client()
+            msg = f"*WARNING: new data for {expected_date} is still not available!*"
+            client.chat_postMessage(channel="#cron_errors", text=msg)
         return pandas.to_datetime(df.columns).max().date()
 
     def launch_job(self, **kwargs):
