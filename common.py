959bed6879bfbe067176f8a6a9b204bc0a119ceb
lematt1991 <lematt1991@gmail.com>
2021-03-09 14:39:02 -0800

    Cleaning up common.py

--- common.py
+++ common.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-import h5py
 import numpy as np
 import pandas as pd
-import torch as th
 from numpy.linalg import norm
-import itertools
-from collections import defaultdict
-import datetime
 import os
 import re
 from lib import cluster
@@ -23,62 +18,6 @@ def update_repo(repo):
     check_call(["git", "checkout", "master"], cwd=data_pth)
     check_call(["git", "pull"], cwd=data_pth)
     return data_pth
-
-
-def drop_k_days(dset, outfile, days):
-    # We'll need to rename everything in case the number of events for a given dimension
-    # drops to 0 after filtering.
-    counter = itertools.count()
-    namer = defaultdict(counter.__next__)
-    ds_dt = h5py.special_dtype(vlen=np.dtype("int"))
-    ts_dt = h5py.special_dtype(vlen=np.dtype("float32"))
-
-    with h5py.File(dset, "r") as hin, h5py.File(outfile, "w") as hout:
-        times = hin["time"][:]
-        nodes = hin["node"][:]
-        new_nodes = []
-        new_times = []
-        # Compute the new maxtime
-        max_time = np.max([ts[-1] for ts in times]) - days
-
-        processed = np.array([])
-        all_times = []
-        all_nodes = []
-
-        for idx, ts in enumerate(times):
-            mask = ts < max_time
-            if not mask.any():
-                continue
-            new_nodes.append(np.array([namer[x] for x in nodes[idx][mask]]))
-            new_times.append(ts[mask])
-
-            mask = ~np.in1d(new_nodes[-1], processed)
-            all_nodes.append(new_nodes[-1][mask])
-            all_times.append(new_times[-1][mask])
-            processed = np.concatenate([processed, np.unique(all_nodes[-1])])
-
-        print(new_times)
-        old_idxs, new_idxs = map(np.array, zip(*namer.items()))
-        hout["nodes"] = hin["nodes"][:][old_idxs[sorted(new_idxs)]]
-        _time = hout.create_dataset("time", (len(new_times),), dtype=ts_dt)
-        _node = hout.create_dataset("node", (len(new_nodes),), dtype=ds_dt)
-        _time[:] = new_times
-        _node[:] = new_nodes
-        hout["cascades"] = np.arange(len(new_nodes))
-        if "basedate" in hin.attrs:
-            date = datetime.datetime.strptime(hin.attrs["basedate"], "%Y-%m-%d")
-            hout.attrs["basedate"] = str((date - datetime.timedelta(days=days)).date())
-            hout.attrs["gt_basedate"] = hin.attrs.get("gt_basedate", str(date))
-
-        if "ground_truth" in hin.keys():
-            hout["ground_truth"] = hin["ground_truth"][:][old_idxs[sorted(new_idxs)]]
-
-        all_times = np.concatenate(all_times)
-        idx = all_times.argsort()
-        all_times = all_times[idx]
-        all_nodes = np.concatenate(all_nodes)[idx]
-        hout["all_nodes"] = all_nodes
-        hout["all_times"] = all_times
 
 
 def drop_k_days_csv(dset, outfile, days):
@@ -103,66 +42,7 @@ def smooth_csv(indset: str, outdset: str, days: int):
     smooth.cumsum(0).transpose().to_csv(outdset)
 
 
-def smooth_h5(indset: str, outdset: str, smooth_days: int):
-    with h5py.File(indset, "r") as hin, h5py.File(outdset, "w") as hout:
-        for k, v in hin.attrs.items():
-            hout.attrs[k] = v
-        nodes, times, all_nodes, all_times = [], [], [], []
-        nnodes = hin["nodes"].shape[0]
-        hout["nodes"] = hin["nodes"][:]
-        hout["cascades"] = hin["cascades"][:]
-        hout["ground_truth"] = hin["ground_truth"][:]
-        processed = set()
-        for ns, ts in zip(hin["node"][:], hin["time"][:]):
-            # Compute number of events of each type for each day
-            current_nodes, current_times = [], []
-            days = np.floor(ts).astype(int)
-            ndays = days.max() + 1
-            events = np.bincount(
-                ns + (days * nnodes), minlength=ndays * nnodes
-            ).reshape(-1, nnodes)
-            smoothed = np.ceil(
-                pd.DataFrame(events)
-                .rolling(window=smooth_days, min_periods=1)
-                .mean()
-                .values
-            ).astype(int)
-            for node in range(nnodes):
-                for day in range(ndays):
-                    if events[day, node] <= 0:
-                        continue
-                    current_times.append(
-                        np.random.uniform(day, day + 1, smoothed[day, node])
-                    )
-                    current_nodes.append(
-                        np.full(current_times[-1].shape, node, dtype="int")
-                    )
-                    if node not in processed:
-                        all_nodes.append(current_nodes[-1])
-                        all_times.append(current_times[-1])
-            ts_ = np.concatenate(current_times)
-            idx = np.argsort(ts_)
-            nodes.append(np.concatenate(current_nodes)[idx])
-            times.append(ts_[idx])
-        x = hout.create_dataset(
-            "node", dtype=h5py.special_dtype(vlen=np.dtype("int")), shape=(len(nodes),)
-        )
-        x[:] = nodes
-        x = hout.create_dataset(
-            "time",
-            dtype=h5py.special_dtype(vlen=np.dtype("float32")),
-            shape=(len(nodes),),
-        )
-        x[:] = times
-        all_times = np.concatenate(all_times)
-        idx = np.argsort(all_times)
-        hout["all_times"] = all_times[idx]
-        hout["all_nodes"] = np.concatenate(all_nodes)[idx]
-
-
-def smooth(indset: str, outdset: str, days: int):
-    smoother = smooth_csv if indset.endswith(".csv") else smooth_h5
-    smoother(indset, outdset, days)
+smooth = smooth_csv
 
 
 def print_model_stats(mus, beta, S, U, V, A):
