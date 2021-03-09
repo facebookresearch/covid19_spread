3e7de485d76c58ff232756396293af97cc137138
lematt1991 <lematt1991@gmail.com>
2021-03-09 12:31:19 -0800

    Refactoring usa/convert.py

--- data/usa/convert.py
+++ data/usa/convert.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 from os import listdir
 from os.path import isfile, join
 from process_cases import SOURCES
+import warnings
 
 # from process_cases import get_nyt
 
@@ -63,73 +64,52 @@ def read_population():
 
 
 def process_time_features(df, pth, shift=0, merge_nyc=False, input_resolution="county"):
-    mobility = pd.read_csv(pth)
-    dates = pd.to_datetime(mobility.columns[2:])
-    dates = dates[np.where(dates >= df.columns.min())[0]]
-    mobility = mobility[
-        mobility.columns[:2].to_list()
-        + list(map(lambda d: d.strftime("%Y-%m-%d"), dates))
-    ]
-
-    print(np.unique(mobility["type"]))
-    n_mobility_types = len(np.unique(mobility["type"]))
-    mobility_types = {r: v for (r, v) in mobility.groupby("region")}
+    time_features = pd.read_csv(pth)
+    if input_resolution == "county_state":
+        # Expand state level time features to each county in `df`
+        idx = df.rename_axis("county").reset_index()[["county"]]
+        idx["region"] = idx["county"].apply(lambda x: x.split(", ")[-1])
+        time_features = time_features.merge(idx, on="region").drop(columns="region")
+        time_features = time_features.rename(columns={"county": "region"})
+    time_feature_regions = time_features["region"].unique()
+    ncommon = len(df.index.intersection(time_feature_regions))
+    if ncommon != len(df):
+        missing = set(df.index).difference(set(time_feature_regions))
+        warnings.warn(
+            f"{pth}: Missing time features for the following regions: {list(missing)}"
+        )
+    if ncommon != len(time_feature_regions):
+        ignoring = set(time_feature_regions).difference(set(df.index))
+        warnings.warn(
+            f"{pth}: Ignoring time features for the following regions: {list(ignoring)}"
+        )
+        time_features = time_features[time_features["region"].isin(set(df.index))]
     if merge_nyc:
-        mobility = merge_nyc_boroughs(mobility, n_mobility_types)
-    print(f"Lengths df={len(df)}, mobility_types={len(mobility_types)}")
-    print(mobility.head(), len(mobility), n_mobility_types)
-
-    mob = {}
-    skipped = 0
-    print(dates.min(), dates.max(), df.columns)
-    start_ix = np.where(dates.min() == df.columns)[0][0]
-    print(start_ix)
-    start_ix += shift
-    print(start_ix)
-    # FIXME: check via dates between google and fb are not aligned
-    # end_ix = np.where(dates.max() == df.columns)[0][0]
-    end_ix = start_ix + len(dates)
-    end_ix = min(end_ix, df.shape[1])
-    for region in df.index:
-        if input_resolution == "county_state":
-            _, query = region.split(", ")
-        else:
-            query = region
-        if query not in mobility_types:
-            # print(region)
-            skipped += 1
-            continue
-        _m = th.zeros(df.shape[1], n_mobility_types)
-        _v = mobility_types[query].sort_values(by="type").iloc[:, 2:].transpose()
-        _v = _v[: end_ix - start_ix]
-        try:
-            _m[start_ix:end_ix] = th.from_numpy(_v.values)
-            if end_ix < df.shape[1]:
-                _m[end_ix:] = _m[end_ix - 1]
-        except Exception as e:
-            print(region, query, _m.size(), _v.shape)
-            print(_v)
-            raise e
-        assert (_m == _m).all()
-        mob[region] = _m
+        time_features = merge_nyc_boroughs(
+            time_features, len(time_features["type"].unique())
+        )
+    # Transpose to have two level columns (region, type) and dates as index
+    time_features = time_features.set_index(["region", "type"]).transpose().sort_index()
+    time_features.index = pd.to_datetime(time_features.index)
+    # Trim prefix if it starts before the dates in `df`
+    time_features = time_features.loc[time_features.index >= df.columns.min()]
+    # Fill in dates that are missing in `time_features` that exist in `df`
+    time_features = time_features.reindex(df.columns)
+    # Shift time features UP by `shift` days
+    time_features = time_features.shift(shift)
+    # forward fill the missing values
+    time_features = time_features.fillna(method="ffill")
+    # Fill the beginning end with zeros if null
+    time_features = time_features.fillna(0)
+    time_features = time_features[time_features.columns.sort_values()]
+    feature_tensors = {
+        region: th.from_numpy(time_features[region].values)
+        for region in time_features.columns.get_level_values(0).unique()
+    }
     if input_resolution == "county_state":
         pth = pth.replace("state", "county_state")
-    th.save(mob, pth.replace(".csv", ".pt"))
-    print(skipped, df.shape[0])
-
-
-def process_county_features(df):
-    df_feat = pd.read_csv("features.csv", index_col="region")
-    df_feat = (df_feat - df_feat.min(axis=0)) / df_feat.max(axis=0)
-    feat = {}
-    for region in df.index:
-        _v = df_feat.loc[region]
-        # inc = _v["median_income"]
-        # inc = inc - inc.min()
-        # _v["median_income"] = inc / min(1, inc.max()) * 100
-        # _v = (_v - _v.mean()) / _v.std()
-        feat[region] = th.from_numpy(_v.values)
-    th.save(feat, "county_features.pt")
+    th.save(feature_tensors, pth.replace(".csv", ".pt"))
+    return feature_tensors
 
 
 if __name__ == "__main__":
@@ -213,7 +193,6 @@ if __name__ == "__main__":
         print(adj)
         th.save(th.from_numpy(adj), "state_graph.pt")
 
-    # process_county_features(df)
     if opt.with_features:
         res = opt.resolution
         merge_nyc = opt.metric == "deaths" and res == "county"
@@ -223,7 +202,7 @@ if __name__ == "__main__":
             ("symptom-survey/doctor-visits_smoothed_adj_cli-{}.csv", 2),
             ("symptom-survey/fb-survey_smoothed_wcli-{}.csv", 0),
             ("symptom-survey/fb-survey_smoothed_hh_cmnty_cli-{}.csv", 0),
-            # ("symptom-survey/fb-survey_smoothed_wearing_mask-{}.csv", 5),
+            ("symptom-survey/fb-survey_smoothed_wearing_mask-{}.csv", 5),
             ("fb/mobility_features_{}_fb.csv", 5),
             ("google/mobility_features_{}_google.csv", 5),
         ]:
