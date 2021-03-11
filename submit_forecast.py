7d47a3bd2dffd7ec57f2f6ed8c6303c56ce4c15c
lematt1991 <lematt1991@gmail.com>
2021-03-11 09:19:16 -0800

    Cleaning up submit_forecast

--- submit_forecast.py
+++ submit_forecast.py
@@ -151,12 +151,6 @@ def pivot_forecast(forecast, limit=True):
         # limit forecasts to 41 days
         merged = merged[(merged["date"] - merged["date"].min()).dt.days < 41]
 
-    # std = pandas.read_csv(
-    #     os.path.join(job, "final_model_piv.csv"), parse_dates=["date"]
-    # )
-    # std = format_df(std, "std_dev")
-    # merged = merged.merge(std, on=["date", "loc1", "loc2", "loc3"], how="left")
-    # merged["std_dev"] = merged["std_dev"].fillna(0)
     merged["std_dev"] = 0
     merged = merged[
         [
